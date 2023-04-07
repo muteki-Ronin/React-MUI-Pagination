da0eda67a5a29c6d66d8c23d5d386c349f900e7d
Ronin <muteki.ronin@gmail.com>
2023-04-07 22:13:05 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// CORE
+import { Routes, Route } from "react-router-dom";
+// PAGES
+import { HomePage } from "./Pages/HomePage";
+import { About } from "./Pages/About";
+import { NotFound } from "./Pages/NotFound";
+// MUI
+import { Container } from "@mui/material";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container maxWidth="md" sx={{ mt: 5 }}>
+      <Routes>
+        <Route index path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Container>
   );
-}
-
-export default App;
+};
---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,76 @@
+// CORE
+import { useState, useEffect } from "react";
+import { Link as RouterLink, useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
+// MUI
+import {
+  Pagination,
+  PaginationItem,
+  TextField,
+  Stack,
+  Link,
+} from "@mui/material";
+
+const BASE_URL = "http://hn.algolia.com/api/v1/search?";
+
+export const HomePage = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  const [posts, setPosts] = useState([]);
+  const [query, setQuery] = useState("react");
+  const [page, setPage] = useState(
+    parseInt(location.search.split("=")[1] || 1)
+  );
+  const [pageQty, setPageQty] = useState(0);
+
+  const handleChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}query=${query}&page=${page - 1}`).then(({ data }) => {
+      setPosts(data.hits);
+      setPageQty(data.nbPages);
+      if (data.nbPages < page) {
+        setPage(1);
+        navigate("/");
+      }
+    });
+  }, [query, page, navigate]);
+
+  return (
+    <>
+      <TextField
+        fullWidth
+        label="Query"
+        value={query}
+        onChange={handleChange}
+      />
+      <Stack spacing={2}>
+        {pageQty && (
+          <Pagination
+            count={pageQty}
+            page={page}
+            onChange={(_, num) => setPage(num)}
+            showFirstButton
+            showLastButton
+            sx={{ marginY: 3, marginX: "auto" }}
+            renderItem={(item) => (
+              <PaginationItem
+                component={RouterLink}
+                to={`/?page=${item.page}`}
+                {...item}
+              />
+            )}
+          />
+        )}
+        {posts.map((post) => (
+          <Link key={post.objectID} href={post.url}>
+            {post.title || post.story_title}
+          </Link>
+        ))}
+      </Stack>
+    </>
+  );
+};
